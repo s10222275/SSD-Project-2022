3469ea02bc434a14d5c0d07581a710c2f443db44
Jia Hui Goh <s10222567@connect.np.edu.sg>
2022-06-30 23:37:37 +0800

    created ApplicationRole class

---
+++ SSD Project/Models/ApplicationRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace SSD_Project.Models
+
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string IPAddress { get; set; }
+    }
+}
