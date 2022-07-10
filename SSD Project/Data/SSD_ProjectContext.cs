099b27e974eb398ba7bb75d964bbad07fc36b265
Jia Hui Goh <s10222567@connect.np.edu.sg>
2022-07-10 14:53:28 +0800

    fixed ig

--- SSD Project/Data/SSD_ProjectContext.cs
+++ SSD Project/Data/SSD_ProjectContext.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SSD_Project.Models;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 
--- SSD Project/Models/ApplicationRole.cs
+++ SSD Project/Models/ApplicationRole.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity;
 
 namespace SSD_Project.Models
 {
