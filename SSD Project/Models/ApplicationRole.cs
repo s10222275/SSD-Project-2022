3242b902164817dd03c7d49d8d8a7c0742dc490f
Jia Hui Goh <s10222567@connect.np.edu.sg>
2022-07-10 13:58:37 +0800

    added applicationrole in isaac2 branch

---
+++ SSD Project/Models/ApplicationRole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SSD_Project.Models
+{
+	public class ApplicationRole :IdentityRole
+	{
+		public string Description { get; set; }
+		public DateTime CreatedDate { get; set; }
+		//default properties(Name) in Identity class
+		//ApplicationRole stores additional role values
+	}
+}
